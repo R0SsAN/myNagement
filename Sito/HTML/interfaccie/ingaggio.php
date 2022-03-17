82b7216cd71569a783753815690310cf6ac4e328
Luca Rossini <66002235+R0SsAN@users.noreply.github.com>
2022-03-17 10:56:48 +0100

    Correzione interfaccia ingaggio
    Correzione estetica dell'interfaccia "ingaggio dipendenti"

--- Sito/HTML/interfaccie/ingaggio.php
+++ Sito/HTML/interfaccie/ingaggio.php
@@ -106,7 +106,7 @@ function Errore($msg)
 <head>
     <meta name="viewport" content="width=device-widtj, initial-scale=1.0" />
     <title>Admin dashboard</title>
-    <link rel="stylesheet" type="text/css" href="sito/dipendenti_style.css" />
+    <link rel="stylesheet" type="text/css" href="../../CSS/dipendenti_style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
@@ -116,91 +116,7 @@ function Errore($msg)
 </head>
 
 <body>
-    <!-- <div class="dipendenti">
-        <form action="" method="POST">
-            <div class="insert">
-                <div class="form">
-                    <div class="input-container ic1">
-                        <input id="firstname" class="input" type="text" placeholder=" " name="firstname" />
-                        <div class="cut"></div>
-                        <label for="firstname" class="placeholder">Nome</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <input id="lastname" class="input" type="text" placeholder=" " name="lastname" />
-                        <div class="cut"></div>
-                        <label for="lastname" class="placeholder">Cognome</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <input id="email" class="input" type="text" placeholder=" " name="email" />
-                        <div class="cut cut-short"></div>
-                        <label for="email" class="placeholder">Email</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <input id="cf" class="input" type="text" placeholder=" " name="cf" />
-                        <div class="cut cut-short"></div>
-                        <label for="email" class="placeholder">Codice fiscale</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <input id="indirizzo" class="input" type="text" placeholder=" " name="indirizzo" />
-                        <div class="cut cut-short"></div>
-                        <label for="email" class="placeholder">Indirizzo</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <input id="indirizzo" class="input" type="text" placeholder=" " name="comune" />
-                        <div class="cut cut-short"></div>
-                        <label for="email" class="placeholder">Comune</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <input id="tel" class="input" type="text" placeholder=" " name="tel" />
-                        <div class="cut"></div>
-                        <label for="firstname" class="placeholder">Numero di telefono</label>
-                    </div>
-                </div>
-                <div class="form">
-                    <div class="input-container ic2">
-                        <div class="cut"></div>
-                        <label for="lastname" class="placeholder">Tipo contratto</label>
-                        <br><br><br>
-                        <input type="radio" name="contratto" onclick="disabilita()"> Indeterminato <br>
-                        <input type="radio" name="contratto" onclick="abilita()"> Determinato
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br><br>
-                        <div style="visibility: hidden;" id="data">
-                            <div class="date">
-                                Data inizio<input type="date" name="inizio">
-                            </div>
-                            <div class="date">
-                                Data fine<input type="date" name="fine">
-                            </div>
-
-                        </div>
-                    </div>
-                    <br><br><br><br>
-                    <div class="input-container ic2">
-                        <input id="mansione" class="input" type="text" placeholder=" " name="mansione" />
-                        <div class="cut cut-short"></div>
-                        <label for="email" class="placeholder">Mansione</label>
-                    </div>
-                    <div class="input-container ic2">
-                        <div class="cut cut-short"></div>
-                        <label for="ora" class="placeholder">Orario</label>
-                        <br><br><br>
-                        <input type="radio" name="orario"> 5H
-                        <input type="radio" name="orario"> 8H
-                    </div>
-                    <br><br>
-                    <div class="input-container ic2">
-                        <div class="cut cut-short"></div>
-                        <label for="salario" class="placeholder">Salario</label>
-                        <br><br><br>
-                        <input type="range" name="stiendio" value="1000" min="800" max="2000" oninput="this.nextElementSibling.value = this.value">
-                        <output>1000</output>&nbsp;€
-                    </div>
-                    <br><br> <br><br>
-                    <button type="text" class="submit">Ingaggia</button>
-                </div>
-            </div>
-        </form>
-    </div> -->
+    
     <script src="https://kit.fontawesome.com/f69c57d50d.js" crossorigin="anonymous"></script>
     <script src="../../JS/dashboard_script.js"></script>
     <script src="sito/ingaggio_script.js"></script>
