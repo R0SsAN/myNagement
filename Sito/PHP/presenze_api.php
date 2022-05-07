4da4c2f5f89d491cefd59e09b1b2769d8e0bb642
TiaNespo <62105891+TiaNespo@users.noreply.github.com>
2022-05-07 12:01:25 +0200

    presenze
    sistemata checkbox presenza

--- Sito/PHP/presenze_api.php
+++ Sito/PHP/presenze_api.php
@@ -9,6 +9,7 @@ if (isset($_POST["genera"])) {
         $sql = "SELECT Nome,Cognome,Cod FROM `dipendenti` WHERE CodAzienda=" . $_SESSION['aziendaId'];
         if ($result = mysqli_query($link, $sql)) {
             if (mysqli_num_rows($result) > 0) {
+                // print("<pre>".print_r(mysqli_num_rows($result),true)."</pre>");
                 $return = "";
                 while ($row = mysqli_fetch_array($result)) {
                     $return .= '<tr>';
@@ -16,7 +17,7 @@ if (isset($_POST["genera"])) {
                     $return .= '<td class="column2">' . $row['Cognome'] . '</td>';
 
                     $sql2 = "SELECT presente FROM `presenza` WHERE CodDipendente=" . $row["Cod"] . " AND giorno='" . date("Y-m-d") . "'";
-
+                    
                     if ($result2 = mysqli_query($link, $sql2)) {
                         if (mysqli_num_rows($result2) > 0) {
                             while ($row2 = mysqli_fetch_array($result2)) {
@@ -29,11 +30,11 @@ if (isset($_POST["genera"])) {
                                                     $return .= '<td class="column3">' . '<input id="check" type="checkbox" disabled="disabled" name="checkpresenza" id="" onclick="SalvaCod(' . $row["Cod"] . ')">' . '</td>';
                                                     $return .= '<td class="column4">' . 'Fino A: ' . $row3['DataFine'] . ' per ' . $row3['Tipo'] . '</td>';
                                                     $return .= '<td class="column5">' . '<button data-bs-toggle="modal" data-bs-target="#myModal" onclick="AggiornaButton(true,' . $row["Cod"] . ')">Modifica Assenza</button>' . '</td>';
-                                                }else if (date("Y-m-d") > $row3['DataFine']) {
+                                                } else if (date("Y-m-d") > $row3['DataFine']) {
                                                     $return .= '<td class="column3">' . '<input  id="check" type="checkbox" name="checkpresenza" id="" onclick="SalvaCod(' . $row["Cod"] . ')">' . '</td>';
                                                     $return .= '<td class="column4">' . '/' . '</td>';
                                                     $return .= '<td class="column5">' . '<button data-bs-toggle="modal" data-bs-target="#myModal" onclick="AggiornaButton(false,' . $row["Cod"] . ')">Aggiungi Assenza</button>' . '</td>';
-                                                }else if (date("Y-m-d") < $row3['DataInizio']) {
+                                                } else if (date("Y-m-d") < $row3['DataInizio']) {
                                                     $return .= '<td class="column3">' . '<input id="check" type="checkbox" name="checkpresenza" id="" onclick="SalvaCod(' . $row["Cod"] . ')">' . '</td>';
                                                     $return .= '<td class="column4">' . 'Assente dal:' . $row3['DataInizio'] . 'Fino A: ' . $row3['DataFine'] . ' per ' . $row3['Tipo'] . '</td>';
                                                     $return .= '<td class="column5">' . '<button data-bs-toggle="modal" data-bs-target="#myModal" onclick="AggiornaButton(false,' . $row["Cod"] . ')">Modifica Assenza</button>' . '</td>';
@@ -69,12 +70,27 @@ if (isset($_POST["genera"])) {
 }
 
 if (isset($_POST["aggiorna"]) && isset($_POST["CodDipendente"])) {
+
     if ($_POST["aggiorna"] == true) {
         $dipendente = $_POST["CodDipendente"];
-        $sql = "UPDATE `presenza` SET `presente`=1 WHERE CodDipendente=$dipendente AND giorno='" . date("Y-m-d") . "'";
-        if (mysqli_query($link, $sql)) {
-            $return = "sesso!";
-            die($return);
+        $sql = "SELECT * FROM `presenza` WHERE CodDipendente=$dipendente AND giorno='" . date("Y-m-d") . "'";
+        if ($result = mysqli_query($link, $sql)) {
+            if (mysqli_num_rows($result) > 0) {
+                $return = "";
+                while ($row = mysqli_fetch_array($result)) {
+                    if ($row['presente'] == 0) {
+                        $sql2 = "UPDATE `presenza` SET `presente`=1 WHERE CodDipendente=$dipendente AND giorno='" . date("Y-m-d") . "'";
+                        if (mysqli_query($link, $sql2)) {
+                            mysqli_close($link);
+                        }
+                    }else{
+                        $sql2 = "UPDATE `presenza` SET `presente`=0 WHERE CodDipendente=$dipendente AND giorno='" . date("Y-m-d") . "'";
+                        if (mysqli_query($link, $sql2)) {
+                            mysqli_close($link);
+                        }
+                    }
+                }
+            }
             mysqli_close($link);
         }
     }
